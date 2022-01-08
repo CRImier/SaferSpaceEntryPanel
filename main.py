e3c211f0b58b6a4bf84d5a1fa693a343baf2334c
CRImier <crimier@yandex.ru>
2022-01-08 23:25:57 +0200

    Work finished on everything except endpoint sending, a 6-location panel works perfectly, "clear" can be reused for a 7th location
    + added fix for "ghost keypresses while wifi is enabled" - see here https://crimier.wordpress.com/2021/03/27/ttp229-ghost-keypresses-because-of-esp32-wifi/

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import json
-import requests
 import network
+from time import sleep, time, sleep_ms
+import urequests as requests
 from machine import Pin
-from TTP229_BSF import Keypad
+from ttp229_bsf import Keypad
 
 ## TODO: some docs, perhaps
 # what to add if you want to support more than one keypad?
@@ -14,24 +15,54 @@ from TTP229_BSF import Keypad
 working_led = Pin(2, Pin.OUT)
 working_led.value(False) # low = LED lit
 
+# timeout after last keypress when keypad resets to its idle state (i.e. user started input and then went away)
+key_timeout = 10
+press_tick_timeout = 3
+
+wifi_is_setup = False
+wlan = None
+ssid = ""
+psk = ""
+endpoint = ""
+
 # let's read the config
 # we get the WiFi SSID, WiFi password, and HTTP endpoint from it
-with open("config.json", "r") as f:
-    config = json.load(f)
 
-ssid = config["ssid"]
-psk = config["psk"]
-endpoint = config["endpoint"]
+config = None
 
-# setup WiFi - disable built-in AP and enable the STA interface
+def load_config():
+    global config
+    try:
+        with open("config.json", "r") as f:
+            config = json.load(f)
+    except:
+        print("No WiFi config or wrong WiFi config file format")
 
-wlan = network.WLAN(network.AP_IF)
-wlan.active(False)
+def setup_wifi():
+    global wifi_is_setup, wlan
+    wifi_is_setup = False
+    if config is None:
+        return
 
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
+    ssid = config["ssid"]
+    psk = config["psk"]
 
-wlan.connect(ssid, psk)
+    # setup WiFi - disable built-in AP and enable the STA interface
+
+    wlan = network.WLAN(network.AP_IF)
+    wlan.active(False)
+
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+
+    wlan.connect(ssid, psk)
+    wifi_is_setup = wlan.isconnected()
+
+    sleep(1)
+    # wifi needs to be disabled to not interfere with the touch pads
+    # on bootup, we should indicate the network connectivity somewhat
+    wlan.active(False)
+
 
 # useful later on if we need to reconnect to WiFi
 wlan_reset_seconds = 5
@@ -41,44 +72,59 @@ wlan_connect_seconds = 20
 # Hardware definitions
 ######################
 
-kp = Keypad(sclk, sdo, inputs=16)
+sclk = Pin(22, Pin.OUT)
+sdo = Pin(21, Pin.IN)
+kp = Keypad(sclk, sdo, inputs=16, multi=True)
 
 # we have 3 595 shift registers
 shift_register_count = 3
 
-# 595 SPI interface and pins
-spi = SPI(1, sck=Pin(21), mosi=Pin(13), miso=Pin(12),
-  baudrate=20*1000*1000, polarity=0, phase=0)
-cs = Pin(11, Pin.OUT)
+# 595 interface and pins
+din = Pin(25, Pin.OUT)
+clk = Pin(27, Pin.OUT)
+latch = Pin(26, Pin.OUT)
+
+def shift_out(data):
+    latch.off()
+    for i in range(shift_register_count):
+        byte = data[i]
+        for j in range(8):
+            value = byte & 1<<(7-j)
+            din.value(value)
+            clk.on()
+            sleep_ms(1)
+            clk.off()
+    latch.on()
 
 # LEDs connected to chained 595 ICs
 # LEDs with two-pin tuple are dual-color LEDs where the color of the LED depends on the voltage polarity applied
 # The only real benefit of those LEDs is that they're physically a single package and they might look better in terms of UX
-# But I like them and I have a small package of those LEDs, so that's why I'm using them, even if it hurts repeatability a bit.
+# But I like them and I have a small package of those LEDs, so that's why I'm using them. You can easily replace them with two LEDs!
 # Anyway, the code and the wiring allows for other LEDs to be used, with zero code modifications.
-# The dual-color LEDs have to be wired in such a way where setting pin0 to high and pin1 to low would indicate "True" (i.e. green_
+# The dual-color LEDs have to be wired in such a way where setting pin0 to high and pin1 to low would indicate "True" (i.e. green)
 # and setting pin0 to low and pin1 to high would indicate "False" (i.e. red).
-led_network_act = (0, 1)
-led_status = (2, 3)
+#led_network_act = (0, 1)
+led_status = (6, 7)
 led_guests = (4, 5)
-led_locations = (6, 7)
-led_submittable = (22, 23)
-# and these are single-pin LEDs used for highlighting different status buttons when they're selected
-led_leaving = 8
-leds_time = (9, 10, 11)
-leds_guests = (12, 13, 14, 15)
-leds_locations = (16, 17, 18, 19, 20, 21)
+led_locations = (2, 3)
+led_submittable = (0, 1)
+# These are single-pin LEDs used for highlighting different status buttons when they're selected
+# I've used green LEDs
+led_leaving = 18
+leds_time = (19, 17, 16)
+leds_guests = (22, 23, 21, 20)
+leds_locations = (10, 11, 9, 8, 12, 13)
 
 # Keys connected to the capacitative touch controller
 # 4 status keys - "leaving" and time interval keys, all of these
-key_leaving = 0
-keys_time = (1, 2, 3)
+key_leaving = 6
+keys_time = (4, 3, 1)
 # Keys for marking the amount of guests brought
-keys_guests = (4, 5, 6, 7)
-keys_locations = (8, 9, 10, 11, 12, 13)
+keys_guests = (7, 5, 2, 0)
+keys_locations = (8, 10, 13, 15, 9, 11)
 
-key_submit = 14
-key_clear = 15
+key_submit = 12
+key_clear = 14
 
 #######################
 # LED and 595 functions
@@ -126,9 +172,7 @@ def boolean_switch_led(led, state):
 def update_leds():
     # shifts the LED data out to the 595
     # until this function is called, none of the functions above will actually change the LEDs
-    cs.off()
-    spi.write(shift_reg_data)
-    cs.on()
+    shift_out(shift_reg_data)
 
 #######################
 # Main keypad algorithm
@@ -147,7 +191,7 @@ states = {"leaving":False, "time":None, "guests":None, "locations":[]}
 def reset_state():
     boolean_switch_led(led_status, False)
     boolean_switch_led(led_submittable, False)
-    disable_led(led_network_act)
+    #disable_led(led_network_act)
     disable_led(led_leaving)
     disable_led(led_guests)
     disable_led(led_locations)
@@ -189,14 +233,14 @@ def process_status_press(key):
     else: # key is one of the "time" keys
         # whichever "time" key is pressed, we need to disable the LED that lights up the currently selected "time" key
         if states["time"] is not None:
-            led_index = keys_status.index(states["time"])
-            disable_led(leds_status[led_index])
+            led_index = keys_time.index(states["time"])
+            disable_led(leds_time[led_index])
         if key == states["time"]: # key pressed is the same as the currently pressed "time" key, we interpret it as "cancel"
             states["time"] = None
         else: # pressed key is not the same that was pressed before, new time picked, let's enable the LED that corresponds to it
             states["time"] = key
-            led_index = keys_status.index(states["time"])
-            enable_led(leds_status[led_index])
+            led_index = keys_time.index(states["time"])
+            enable_led(leds_time[led_index])
     # From here on, we'll assume that something has changed
     # If the "status" state isn't valid, we need to show the user that they need to pick more options
     # which is to say, we disable all LEDs and set the "status" LED to red (or whatever its "wrong state" color is)
@@ -216,8 +260,8 @@ def process_status_press(key):
         boolean_switch_led(led_locations, True if states["locations"] else False)
     else:
         # if "leaving", people might pick time/location/guests options, but they're not required to
-        # so only mark them as "okay" if something's selected there, no need to mark them as "wrong" if nothing is selected
-        disable_led(led_guests) if states["guests"] is not None else boolean_switch_led(led_guests, True)
+        # so only mark them as "okay" if something's selected there, no need to mark them as "wrong" if "leaving" is selected
+        disable_led(led_guests) if states["guests"] is None else boolean_switch_led(led_guests, True)
         disable_led(led_locations) if not states["locations"] else boolean_switch_led(led_locations, True)
     # now, in the end, we need to check if the current data is submittable
     boolean_switch_led(led_submittable, determine_submittable())
@@ -265,16 +309,25 @@ def process_locations_press(key):
 
 def process_submit_press():
     submittable = determine_submittable()
-    if not submittable():
+    if not submittable:
         # data doesn't make sense yet, doing nothing
-        return # perhaps we could even blink the red LED at the "submittable" button, but, I guess, that's to be done later.
+        for i in range(5):
+            disable_led(led_submittable)
+            update_leds()
+            sleep(0.1)
+            boolean_switch_led(led_submittable, False)
+            update_leds()
+            sleep(0.1)
+        return
     # let's try and submit it
-    boolean_switch_led(led_network_act, True)
+    #boolean_switch_led(led_network_act, True)
     result = send_data()
     if not result:
-        boolean_switch_led(led_network_act, False)
+        # send failed?
+        # TODO: store data and resend at earliest convenience
+        #boolean_switch_led(led_network_act, False)
         sleep(1)
-    # TODO: store data and resend at earliest convenience
+    reset_state()
 
 def process_clear_press():
     reset_state()
@@ -294,41 +347,63 @@ def get_request_data():
     # the input is "key pin number as it's connected to the TTP229 IC", so, could be 3, 10 or 16, depends on the wiring
     # the output is "index of the time option selected", so, 0, 1 or 2 (as we have 3 time options)
     # since, of course, the endpoint won't know which key is connected to which time button, and arguably it shouldn't matter.
-    d["time"] = keys_time.index(states["time"])
-    d["guests"] = keys_guests.index(states["guests"])
-    d["locations"] = keys_locations.index(states["locations"])
+    d["time"] = keys_time.index(states["time"]) if states["time"] else None
+    d["guests"] = keys_guests.index(states["guests"]) if states["time"] else None
+    d["locations"] = [keys_locations.index(s) for s in states["locations"]]
     return d
 
 def send_data():
+    if not wifi_is_setup:
+        print("Cannot send data - WiFi not set up!")
+        return False
     led_state = True
     # if we're currently not connected, let's "power cycle" the WiFi peripheral and try to reconnect
     if not wlan.isconnected():
-        wlan.active(False)
-        sleep(wlan_reset_seconds)
+        if wlan.active():
+            wlan.active(False)
+            sleep(wlan_reset_seconds)
         wlan.active(True)
-        wlan.connect(ssid, psk)
+        wlan.connect(config["ssid"], config["psk"])
         connect_counter = 0
         while not wlan.isconnected():
             led_state = not led_state
-            boolean_switch_led(led_network_act, led_state)
+            #boolean_switch_led(led_network_act, led_state)
             sleep(1)
             connect_counter += 1
             if connect_counter == wlan_connect_seconds:
                 break
         if not wlan.isconnected(): return False
+        wlan.active(False)
     # presumably, we're connected.
-    boolean_switch_led(led_network_act, True)
+    #boolean_switch_led(led_network_act, True)
     data = get_request_data()
+    if "data" in config:
+        data.update(config["data"])
     try:
-        r = requests.get(endpoint, data=data)
+        pass #r = requests.get(config["endpoint"], data=data)
     except:
-        boolean_switch_led(led_network_act, False)
-        disable_led(led_working)
+        #boolean_switch_led(led_network_act, False)
+        working_led.value(True)
         raise
     else:
+        return True
         if r.status_code == 200:
             return True
     return False
+
+# mock function - as there's nowhere to send data to at the moment, I can just blink LEDs and clear
+# this function overrides the previously defined send_data until commented out or renamed
+def send_data():
+    for i in range(5):
+        disable_led(led_submittable)
+        update_leds()
+        sleep(0.15)
+        boolean_switch_led(led_submittable, True)
+        update_leds()
+        sleep(0.15)
+    disable_led(led_submittable)
+    update_leds()
+    return True
 
 ############################
 # main loop - key processing
@@ -338,38 +413,60 @@ pressed_key_dict = {i:False for i in range(16)}
 
 # this loop processes keypresses and calls the functions appropriate for each key
 # that's basically it. I guess later on it could do something else?
-while True:
-    # mind you, keypad driver is set to only process a single keypress, but that's okay, as this code might be useful later.
-    # we also don't process key release events yet, but we can add initial processing code for them, just in case.
-    keys_just_pressed = []
-    keys_just_released = []
-    pressed_key_list = kp.read()
-    for key, value in pressed_key_dict:
-        if key in pressed_key_list and not value:
-            # key just pressed
-            pressed_key_dict[key] = True
-            keys_just_pressed.append(key)
-        elif key not in pressed_key_list and value:
-            # key just released
-            pressed_key_dict[key] = False
-            keys_just_released.append(key)
-    # now processing separate button types specifically
-    for key in keys_just_pressed:
-        # mind you, in single keypress mode, we'll always be only processing one key at a time,
-        # and this code will work for that either way.
-        if key == key_leaving or key in keys_time:
-            process_status_key(key)
-        elif key in keys_guests:
-            process_guests_key(key)
-        elif key in keys_locations:
-            process_locations_key(key)
-        elif key == key_submit:
-            process_submit_key()
-        elif key == key_clear:
-            process_clear_key()
-        else:
-            print("Unprocessable key: {}".format(key))
-    # no processing for just-released keys, basically, the code above is all you need for now.
-    # "just released" can later be used for debouncing, for instance.
-    # but then, so can this, lol:
-    sleep(1)
+def main():
+    last_press_time = None
+    last_press_ticks = 0
+    while True:
+        # mind you, keypad driver is set to only process a single keypress, but that's okay, as this code might be useful later.
+        # we also don't process key release events yet, but we can add initial processing code for them, just in case.
+        keys_just_pressed = []
+        keys_just_released = []
+        pressed_key_list = kp.read()
+        if pressed_key_list and last_press_ticks < press_tick_timeout:
+            #print("Discarded keypress: ", pressed_key_list)
+            continue
+        for key, value in pressed_key_dict.items():
+            if key in pressed_key_list and not value:
+                # key just pressed
+                print("Pressed: ", key)
+                pressed_key_dict[key] = True
+                keys_just_pressed.append(key)
+                print(last_press_ticks)
+                last_press_ticks = 0
+            elif key not in pressed_key_list and value:
+                # key just released
+                #print("Released: ", key)
+                pressed_key_dict[key] = False
+            #    keys_just_released.append(key)
+        # now processing separate button types specifically
+        if keys_just_pressed:
+            print("Pressed: ", keys_just_pressed)
+            last_press_time = time()
+        for key in keys_just_pressed:
+            # mind you, in single keypress mode, we'll always be only processing one key at a time,
+            # and this code will work for that either way.
+            if key == key_leaving or key in keys_time:
+                process_status_press(key)
+            elif key in keys_guests:
+                process_guests_press(key)
+            elif key in keys_locations:
+                process_locations_press(key)
+            elif key == key_submit:
+                if process_submit_press():
+                    reset_state()
+            elif key == key_clear:
+                process_clear_press()
+            else:
+                print("Unprocessable key: {}".format(key))
+        # last keypress more than X seconds ago? resetting
+        if last_press_time and time()-last_press_time > key_timeout:
+            #print("Keypress timeout")
+            reset_state()
+            last_press_time = None
+        # 
+        last_press_ticks += 1
+
+reset_state()
+load_config()
+setup_wifi()
+main()
